e9a1103382f083b0674e4cd49bfc8964fb45c696
CesarLinux <eduardomelchor34@gmail.com>
2025-01-22 19:39:42 -0600

    seteando diferentes apis

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,11 @@
 export const getGifs = async( category ) => {
-  const apiKey = 'P9KFYBzZOeUhMNsS45fzSnfwBGvoh3qz';
+  
+  const apiKey = 'iGNeAiKwv78T0tAVOuAOJxanrRY336W0';
+  // const apiKey = 'P9KFYBzZOeUhMNsS45fzSnfwBGvoh3qz';
+  // const apiKey = 'pIRvOhYwpnwTNpLzuW14HyrrAR54YPVr';
+  
+
+
   const url = `https://api.giphy.com/v1/gifs/search?api_key=${ apiKey }&q=${ category }&limit=10`;
   const resp = await fetch( url );
   const { data } = await resp.json();
