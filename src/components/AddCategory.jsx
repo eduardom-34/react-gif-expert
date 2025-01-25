11923cebc9d2c2789a51937eee6bd05aba3f6edb
CesarLinux <eduardomelchor34@gmail.com>
2025-01-24 20:03:28 -0600

    Pruebas del componente - AddCategory

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import { useState } from "react"
 
 export const AddCategory = ({ onNewCategory }) => {
@@ -33,3 +34,9 @@ export const AddCategory = ({ onNewCategory }) => {
     </form>
   )
 }
+
+AddCategory.propTypes = {
+  onNewCategory: PropTypes.func.isRequired,
+};
+
+
