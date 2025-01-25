a4226c1bb5a6357fda896c78f237fa42d045b7a0
CesarLinux <eduardomelchor34@gmail.com>
2025-01-24 20:54:05 -0600

    Pruenas del componente GifGrid - Mocj customHool

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,5 +1,6 @@
 import { GifItem } from "./GifItem";
 import { useFetchGifs } from "../hooks/useFetchGifs";
+import PropTypes from "prop-types";
 
 export const GifGrid = ({ category }) => {
 
@@ -10,7 +11,7 @@ export const GifGrid = ({ category }) => {
     <>
     <h3>{ category }</h3>
     {
-      isLoading && (<h2 >Cargano...</h2>)
+      isLoading && (<h2 >Cargando...</h2>)
     }
     
 
@@ -28,4 +29,8 @@ export const GifGrid = ({ category }) => {
 
     </>
   )
+}
+
+GifGrid.propTypes = {
+  category: PropTypes.string.isRequired,
 }
---
+++ test/components/GifGrid.test.jsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import { GifGrid } from "../../src/components/GifGrid";
+
+describe('Pruebas en <GifGrid />', () => {
+
+  const category = 'One Punch';
+  
+  test('debe de mostrar el loading inicialmente', () => {
+
+    render( <GifGrid category={ category } /> );
+    expect( screen.getByText('Cargando...') );
+    expect( screen.getByText( category ) );
+    
+  });
+
+  test('debe de mostrar items cuando se cargan las imagenes useFetchGifs', () => {
+    
+  });
+  
+  
+
+});
